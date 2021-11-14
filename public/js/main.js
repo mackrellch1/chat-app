8ce6fe4a6c094368e49c4c4f6192add03673c1d5
cmackrell <colin.mackrell99@gmail.com>
2021-11-14 13:44:36 -0500

    initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,23 @@
 const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
+
+// Get username and room from url
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
 
 const socket = io()
 
+// Join chat room
+socket.emit('joinRoom', { username, room })
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room)
+    outputUsers(users)
+})
 
 // Message from server
 socket.on('message', message => {
@@ -31,9 +46,19 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message) {
     const div = document.createElement('div')
     div.classList.add('message')
-    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`
     document.querySelector('.chat-messages').appendChild(div)
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = `${users.map(users => `<li>${users.username}</li>`).join('')}`
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -17,25 +18,46 @@ const botName = 'Amplify Chat Bot'
 io.on('connection', socket => {
     console.log('New WS Connection...')
 
-    // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to Amplify Chatting Solutions!'))
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room)
 
-    // Broadcast when a user connects
-    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'))
+        socket.join(user.room)
 
-    // Broadcast when a user disconnects
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to Amplify Chatting Solutions!'))
 
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A user has left the chat'))
+        // Broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`))
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
     })
 
     // Listen for chat message
     socket.on('chatMessage', (msg) => {
-        io.emit('message', formatMessage('USER', msg))
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+    })
+
+    // Broadcast when a user disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`))
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
     })
 })
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
 
